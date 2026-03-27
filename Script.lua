a105227aee116136a40c7141f28c0b3dd04925dd
sladkoeshkaogg-svg <sladkoeshkaogg@gmail.com>
2026-03-27 20:25:12 +0500

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,4 +1,4 @@
-local url = "https://raw.githubusercontent.com/sladkoeshkaogg-svg/GGOG-HUB/main/GGOG.lua"
+local url = "https://raw.githubusercontent.com/sladkoeshkaogg-svg/TestGGOGScript/main/Main.lua"
 
 local request = (syn and syn.request) or (http and http.request) or http_request or request
 
